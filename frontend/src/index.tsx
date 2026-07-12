224c4dd68c2cb25f5087b0d2f877c21ec443558c
hellohirusha <randombage.hirusha@gmail.com>
2026-07-12 07:57:19 +0530

    feat(frontend): set up Apollo Client with auth and error links
    Configure the Apollo client (JWT auth link, 401 refresh error link) and mount ApolloProvider, using the Apollo Client v4 API.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client/react";
+import { Toaster } from "react-hot-toast";
+import { apolloClient } from "./lib/apollo";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
